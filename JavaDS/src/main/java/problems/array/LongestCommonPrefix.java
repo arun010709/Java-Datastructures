cf3533120f6e8936fd776425fb4578d3243915bd
Arun <arun010709@gmail.com>
2026-05-06 20:21:41 +0530

    Update LongestCommonPrefix.java

--- JavaDS/src/main/java/problems/array/LongestCommonPrefix.java
+++ JavaDS/src/main/java/problems/array/LongestCommonPrefix.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LongestCommonPrefix {
     public static void main(String args[]){
-        String[] strs={"tower","flow","tight"};
+        String[] strs={"zoo","zoom","zoomla"};
         System.out.print(longestCommonPrefix(strs));
      }
     public static String longestCommonPrefix(String[] strs){
